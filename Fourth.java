59af48e97693285d89986e1fbc185958587b0bfd
kinga <oliwia14327@gmail.com>
2019-10-11 20:07:18 +0200

    Fourth.jva

---
+++ Fourth.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+
+public class Fourth {
+	public static void main(String[] args) {
+
+double[] values={1,2,3};
+
+for(double average : values){
+
+if (average== (1+2+3)/values.length){
+System.out.println(average);
+}}}}
