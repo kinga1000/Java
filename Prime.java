abc37a1ded1920780130d1a741790573c284b072
kinga <oliwia14327@gmail.com>
2019-10-11 20:47:42 +0200

    Prime.java

---
+++ Prime.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Prime {
+	public static void main(String[] args) {
+
+
+System.out.println(2);
+
+System.out.println(3);
+
+System.out.println(5);
+
+System.out.println(7);
+for (int i=2; i<100;i++){
+if(i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0)
+
+
+System.out.println(i);
+}
+}
+}
