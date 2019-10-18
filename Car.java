058da4f0544badbbc63051b1a751b509378c5225
kinga <oliwia14327@gmail.com>
2019-10-18 19:58:48 +0200

    Car

---
+++ Car.java
@@ -0,0 +1,27 @@
+package pl.itacademy.lesson4;
+
+public class Car {
+    String name;
+    String Engine;
+    Double fuelCapacity;
+    Double remainingFuel;
+    Boolean isStarted;
+
+
+
+    void vwstart(boolean isStarted) {
+        if (isStarted) {
+            System.out.println("Diesel engine has been started");
+        } else {
+            System.out.println("Diesel engine has been stopped");
+        }
+        Double km;
+        Double fuelCapacity = 50.0;
+        Double fuelperkm=0.2;
+
+
+
+        }
+
+
+    }
