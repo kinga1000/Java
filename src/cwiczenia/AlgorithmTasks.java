21b5a7d040e7ffef865007607099b35569c099a0
kinga <oliwia14327@gmail.com>
2019-12-03 20:00:16 +0100

    src

--- src/cwiczenia/AlgorithmTasks.java
+++ src/cwiczenia/AlgorithmTasks.java
@@ -1,4 +1,5 @@
 package cwiczenia;
+
 import java.util.*;
 
 
@@ -7,10 +8,10 @@ public class AlgorithmTasks {
 
         Set<Character> words = new TreeSet<>();
 
-        String s= "This is a test text";
-        char[] letters=s.toCharArray();
-        for(char s2: letters){
-            if(s2!=' '){
+        String s = "This is a test text";
+        char[] letters = s.toCharArray();
+        for (char s2 : letters) {
+            if (s2 != ' ') {
                 words.add(s2);
             }
 
@@ -18,24 +19,34 @@ public class AlgorithmTasks {
 
         System.out.println(words.toString());
 
-        List<Character> words2 = new ArrayList<>();
+        //private static void print(String s){
+          //  char[]chars=s.toCharArray();
+          //  Map<Character, Integer> print2=new HashMapp<>();
+          //  for(char )
 
-        String s3= "This is a test text";
 
-        char[] letters2=s3.toCharArray();
-        for(char s4: letters2){
-            words2.add(s4);
-
+            //uzyj compute lub getordefaultp- no metody
         }
 
+        // List<Character> words2 = new ArrayList<>();
+
+        // String s3= "This is a test text";
+
+        // char[] letters2=s3.toCharArray();
+        // for(char s4: letters2) {
+        //     if (s4 != ' ') {
+        //        words2.add(s4);
+//
+        //  }
+        //}
 
 
-        System.out.println(words2.toString());
-        System.out.println(words2.get(2));
-        System.out.println(Collections.frequency(words2, 'i'));
-        System.out.println(Collections.frequency(words2,'h'));
-        System.out.println(Collections.frequency(words2, 't'));
-        System.out.println(Collections.frequency(words2,'s'));
-        System.out.println(Collections.frequency(words2, 'a'));
-        System.out.println(Collections.frequency(words2,'x'));
-}}
+        //System.out.println(words2.toString());
+
+        //  System.out.println(Collections.frequency(words2, 'i'));
+        // System.out.println(Collections.frequency(words2,'h'));
+        // System.out.println(Collections.frequency(words2, 't'));
+        // System.out.println(Collections.frequency(words2,'s'));
+        // System.out.println(Collections.frequency(words2, 'a'));
+        // System.out.println(Collections.frequency(words2,'x'));
+    }
